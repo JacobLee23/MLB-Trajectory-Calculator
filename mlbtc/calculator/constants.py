6e382cb57f154198c2820a0755b3e7e2f6a37787
Jacob Lee <JLpython@outlook.com>
2022-09-27 17:35:38 -0700

    feat: implement Gauss-Legendre algorithm to approximate PI to n digits of precision

--- mlbtc/calculator/constants.py
+++ mlbtc/calculator/constants.py
@@ -3,7 +3,47 @@
 """
 
 import decimal
+from decimal import Decimal
 import typing
 
 
-Number = typing.Union[int, float, decimal.Decimal]
+Number = typing.Union[int, float, Decimal]
+
+
+def gauss_legendre(precision: int = 100) -> Decimal:
+    """
+
+    :param precision:
+    :return:
+    """
+    with decimal.localcontext() as ctx:
+        ctx.prec = precision + 1
+
+        # Initial value setting
+        a: Decimal = Decimal(1)
+        b: Decimal = 1 / Decimal(2).sqrt(ctx)
+        t: Decimal = 1 / Decimal(4)
+        p: int = 1
+        pi: Decimal = Decimal()
+
+        # (n + 1)th values
+        a_: Decimal
+        b_: Decimal
+        t_: Decimal
+        p_: int
+        pi_: Decimal
+
+        while True:
+            a_ = (a + b) / 2
+            b_ = (a * b).sqrt(ctx)
+            t_ = t - p * (a - a_) ** 2
+            p_ = 2 * p
+            pi_ = (a_ + b_) ** 2 / (4 * t_)
+
+            if pi == pi_:
+                return pi
+
+            a, b, t, p, pi = a_, b_, t_, p_, pi_
+
+
+PI = gauss_legendre()
---
+++ mlbtc/tests/test_calculator/test_constants.py
@@ -0,0 +1,3 @@
+"""
+Tests for :py:mod:`mlbtc.calculator.constants`.
+"""
