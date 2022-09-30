ef17275a820fdffbbef8f75f2fd08f36f0d9d76c
Jacob Lee <JLpython@outlook.com>
2022-09-29 21:30:53 -0700

    test: write unit tests for mlbtc.calculator.trajector.Vector2D

--- mlbtc/tests/test_calculator/test_trajectory.py
+++ mlbtc/tests/test_calculator/test_trajectory.py
@@ -8,9 +8,36 @@ import inspect
 from mlbtc.calculator import trajectory
 from mlbtc.calculator import constants
 from mlbtc.calculator.constants import PI
-from mlbtc.calculator.constants import Number
 
 import pytest
+
+
+@pytest.mark.parametrize(
+    "r, x, y, theta", [
+        (Decimal(2).sqrt(), Decimal(1), Decimal(1), PI / Decimal(4)),
+        (
+                Decimal(13).sqrt(), Decimal(2), Decimal(3),
+                constants.arctangent(Decimal(3) / Decimal(2))
+        ),
+        (Decimal(5), Decimal(3), Decimal(4), constants.arctangent(Decimal(4) / Decimal(3)))
+    ]
+)
+def test_vector2d(r: Decimal, x: Decimal, y: Decimal, theta: Decimal):
+    """
+    Unit tests for :py:class:`mlbtc.calculator.trajectory.Vector2D`.
+    """
+    arguments = {k: Decimal(v) for k, v in locals().items()}
+    for system in trajectory.Vector2D.systems:
+        vector = trajectory.Vector2D(
+            system(
+                **{k: arguments[k] for k in inspect.signature(system).parameters}
+            )
+        )
+
+        for key in arguments:
+            assert float(vector[key]) == pytest.approx(
+                float(arguments[key])
+            ), (system, key, vector)
 
 
 @pytest.mark.parametrize(
@@ -32,7 +59,7 @@ import pytest
     ]
 )
 def test_vector_3d(
-        r: Number, x: Number, y: Number, z: Number, theta: Number, rho: Number, phi: Number
+        r: Decimal, x: Decimal, y: Decimal, z: Decimal, theta: Decimal, rho: Decimal, phi: Decimal
 ):
     """
     Unit tests for :py:class:`mlbtc.calculator.trajectory.Vector3D`.
