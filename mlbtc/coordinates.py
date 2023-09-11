13a8f4625c56e77c25ffa54c5ad42b916d54925d
Jacob Lee <JLpython@outlook.com>
2023-09-11 14:34:47 -0700

    feat: create class to convert between 2D coordinate systems

--- mlbtc/coordinates.py
+++ mlbtc/coordinates.py
@@ -6,6 +6,73 @@ import typing
 import numpy as np
 
 
+class Coordinates2D:
+    r"""
+    +-------------------+-----------------------+-----------------------+
+    | Coordinate System | ``coordinates[0]``    | ``coordinates[1]``    |
+    +===================+=======================+=======================+
+    | Cartesian         | :math:`x`             | :math:`y`             |
+    +-------------------+-----------------------+-----------------------+
+    | Polar             | :math:`r`             | :math:`\theta`        |
+    +-------------------+-----------------------+-----------------------+
+
+    :param coordinates:
+    :param system:
+    """
+    def __init__(self, coordinates: np.ndarray, system: typing.Literal["cartesian", "polar"]):
+        if coordinates.shape != (2,):
+            raise ValueError
+        
+        self._coordinates, self._system = coordinates, system
+
+    def __repr__(self) -> str:
+        arguments = ", ".join(
+            f"{k}={self.__getattribute__(k)}" for k in ["cartesian", "polar"]
+        )
+        return f"{type(self).__name__}({arguments})"
+    
+    @property
+    def cartesian(self) -> np.ndarray:
+        """
+        Converts to the Cartesian coordinate system (:math:`x`, :math:`y`).
+        """
+        # (x, y)
+        if self._system == "cartesian":
+            return self._coordinates
+        # (r, theta)
+        elif self._system == "polar":
+            return np.array(
+                [
+                    # x = r * cos(theta)
+                    self._coordinates[0] * np.cos(self._coordinates[1]),
+                    # y = r * sin(theta)
+                    self._coordinates[0] * np.sin(self._coordinates[1])
+                ]
+            )
+        else:
+            return np.array([np.nan, np.nan])
+        
+    @property
+    def polar(self) -> np.ndarray:
+        r"""
+        Converts to the polar coordinate system (radius :math:`r`, azimuth :math:`\phi`).
+        """
+        # (x, y)
+        if self._system == "cartesian":
+            return np.array(
+                [
+                    # r = sqrt(x ** 2 + y ** 2)
+                    np.sqrt((self._coordinates ** 2).sum()),
+                    # theta = arctan(y / x),
+                    np.arctan(self._coordinates[1] / self._coordinates[0])
+                ]
+            )
+        elif self._system == "polar":
+            return self._coordinates
+        else:
+            return np.array([np.nan, np.nan])
+
+
 class Coordinates3D:
     r"""
     +-------------------+-----------------------+-----------------------+-----------------------+
@@ -117,15 +184,9 @@ class Coordinates3D:
                     # r = sqrt(x ** 2 + y ** 2 + z ** 2)
                     np.sqrt((self._coordinates ** 2).sum()),
                     # theta = arccos(z / sqrt(x ** 2 + y ** 2 + z ** 2))
-                    np.arccos(
-                        self._coordinates[2] / np.sqrt((self._coordinates ** 2).sum())
-                    ),
+                    np.arccos(self._coordinates[2] / np.sqrt((self._coordinates ** 2).sum())),
                     # phi = sign(y) * arccos(x / sqrt(x ** 2 + y ** 2))
-                    np.sign(self._coordinates[1]) * np.arccos(
-                        self._coordinates[0] / np.sqrt(
-                            self._coordinates[0] ** 2 + self._coordinates[1] ** 2
-                        )
-                    )
+                    np.sign(self._coordinates[1]) * np.arccos(self._coordinates[0] / np.sqrt(self._coordinates[0] ** 2 + self._coordinates[1] ** 2))
                 ]
             )
         # (rho, phi, z)
