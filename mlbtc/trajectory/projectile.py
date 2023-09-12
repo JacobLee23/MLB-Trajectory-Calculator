e4b8c4f18be61bc1399436af418cbf2d90efb197
Jacob Lee <JLpython@outlook.com>
2023-09-12 00:37:17 -0700

    refactor: compute kinematic quantities in mlbtc.trajectory.projectile

--- mlbtc/trajectory/projectile.py
+++ mlbtc/trajectory/projectile.py
@@ -3,12 +3,29 @@
 
     Baseball dimension specifications, per the MLB Rulebook (Rule 3.01)
 
-    :type: pd.Series
+    :type: pd.DataFrame
 """
 
 import pandas as pd
+
+from ..coordinates import Vector3D
 
 
 PROJECTILE = pd.DataFrame(
     {"weight": {"min": 5, "max": 5.25}, "circumference": {"min": 9, "max": 9.25}}
 )
+
+
+class Acceleration(Vector3D):
+    """
+    """
+
+
+class Velocity(Vector3D):
+    """
+    """
+
+
+class Position(Vector3D):
+    """
+    """
